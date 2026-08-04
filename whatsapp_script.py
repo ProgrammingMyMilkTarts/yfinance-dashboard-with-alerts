b6b4fa53cd3e784b61b800e606fb5e5fc090a740
Nathan <nathanvdmxd@gmail.com>
2026-08-04 11:57:04 +0200

    close tabs change

--- whatsapp_script.py
+++ whatsapp_script.py
@@ -3,6 +3,6 @@ import pywhatkit
 numbers = ["+27794975184", "+27794975185", "+27794975186"]  # Add more numbers as needed
 
 for number in numbers:
-    pywhatkit.sendwhatmsg_instantly(number, "message")
+    pywhatkit.sendwhatmsg_instantly(number, "message",15,True,3)
 
 print("Message sent successfully!")
