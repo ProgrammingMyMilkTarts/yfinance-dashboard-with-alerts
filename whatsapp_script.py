01ee818427c60f0bfd55923360cf56e43357d47c
Nathan <nathanvdmxd@gmail.com>
2026-08-04 12:56:45 +0200

    just made it cleaner

--- whatsapp_script.py
+++ whatsapp_script.py
@@ -1,8 +1,9 @@
 import pywhatkit
 
-numbers = ["+27794975184", "+27794975185", "+27794975186"]  # Add more numbers as needed
 
-for number in numbers:
-    pywhatkit.sendwhatmsg_instantly(number, "message",15,True,3)
+phone_numbers = ["+27823468282"]  # Add more numbers as needed
+
+for i in phone_numbers:
+    pywhatkit.sendwhatmsg_instantly(i, "Automatic Messager",15,True,3)
 
 print("Message sent successfully!")
