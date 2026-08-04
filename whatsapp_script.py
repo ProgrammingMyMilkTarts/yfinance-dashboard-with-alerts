c030e1aa46ad35019bf3212d63834eeacc0c1821
Nathan <nathanvdmxd@gmail.com>
2026-08-04 11:45:12 +0200

    Installed pywhatkit via venv + made the basic script to send message via pywhatkit + has pywhatkit databse to track all messages

---
+++ whatsapp_script.py
@@ -0,0 +1,8 @@
+import pywhatkit
+
+numbers = ["+27794975184", "+27794975185", "+27794975186"]  # Add more numbers as needed
+
+for number in numbers:
+    pywhatkit.sendwhatmsg_instantly(number, "message")
+
+print("Message sent successfully!")
