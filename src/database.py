f63a75ca7815c51a5e2f31cabc92f9c9f1fad0f6
Nathan <nathanvdmxd@gmail.com>
2026-08-14 01:31:11 +0200

    made the db file to connect to db

---
+++ src/database.py
@@ -0,0 +1,20 @@
+import os
+from sqlalchemy import create_engine
+from dotenv import load_dotenv
+
+def get_db_engine():
+  load_dotenv()
+  
+  # Grab secrets securely from environment variables
+  user = os.getenv('DB_USER')
+  password = os.getenv('DB_PASSWORD')
+  host = os.getenv('DB_HOST', 'localhost')
+  port = os.getenv('DB_PORT', '5432')
+  dbname = os.getenv('DB_NAME', 'financial_db')
+
+  # Create and return the SQLAlchemy engine connection object
+  engine = create_engine(
+      f'postgresql+psycopg2://{user}:{password}@{host}:{port}/{dbname}'
+  )
+
+  return engine
